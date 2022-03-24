7f3eb08c6530bcbca7d5596a3475313ed8412b1d
Владимир Грушевский <v.grushevsky@gkk.guru>
2022-03-24 19:05:10 +0200

    add vault.yml and refactoring

--- vault_decrypt.sh
+++ vault_decrypt.sh
@@ -2,5 +2,4 @@
 
 ansible-vault decrypt roles/postgres_dump/files/.pgpass --vault-id volodya_tm@.pass
 ansible-vault decrypt roles/postgres_dump/files/* --vault-id volodya_tm@.pass
-ansible-vault decrypt roles/postgres_dump/defaults/* --vault-id volodya_tm@.pass
-ansible-vault decrypt roles/postgres_dump/vars/* --vault-id volodya_tm@.pass
+ansible-vault decrypt group_vars/all/vault.yml --vault-id volodya_tm@.pass
--- vault_encrypt.sh
+++ vault_encrypt.sh
@@ -2,5 +2,4 @@
 
 ansible-vault encrypt roles/postgres_dump/files/.pgpass --vault-id volodya_tm@.pass
 ansible-vault encrypt roles/postgres_dump/files/* --vault-id volodya_tm@.pass
-ansible-vault encrypt roles/postgres_dump/defaults/* --vault-id volodya_tm@.pass
-ansible-vault encrypt roles/postgres_dump/vars/* --vault-id volodya_tm@.pass
+ansible-vault encrypt group_vars/all/vault.yml --vault-id volodya_tm@.pass
